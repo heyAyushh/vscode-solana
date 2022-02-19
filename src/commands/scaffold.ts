f388d53e7f3101f62404f2d41661f3a3666035cd
heyAyushh <ayush-15@live.com>
2022-02-20 03:25:53 +0530

    ask for root folder and Jump to the scaffold

--- src/commands/scaffold.ts
+++ src/commands/scaffold.ts
@@ -1,44 +1,75 @@
 import { spawnChan } from "../helpers/spawnExec";
 import * as vscode from 'vscode';
 import { EXT_NAME } from "../config";
-import chan from "../helpers/outputChannel";
+import chan, { appendChan } from "../helpers/outputChannel";
+import path = require("path");
 
 const anchorInit = () => vscode.commands.registerCommand(
   `${EXT_NAME}.scaffold`,
   async () => {
-    const item = await vscode
-      .window
-      .showQuickPick(['init (default)', 'Synthetify/solana-template'], {
-        placeHolder: 'init or synthetify template',
-      });
-    if (!item) {
-      return;
-    }
+    try {
+      const item = await vscode
+        .window
+        .showQuickPick(['init (default)', 'Synthetify/solana-template'], {
+          placeHolder: 'init or synthetify template',
+        });
+      if (!item) {
+        return;
+      }
 
-    const name = await vscode
-      .window
-      .showInputBox({
-        placeHolder: 'Enter Project name: <uwu defi>',
-      });
-    if (!name) {
-      return;
-    }
+      const name = await vscode
+        .window
+        .showInputBox({
+          placeHolder: 'Enter Project name: <uwu defi>',
+        });
+      if (!name) {
+        return;
+      }
 
-    if (item && name) {
+      const options: vscode.OpenDialogOptions = {
+        canSelectMany: false,
+        openLabel: 'Select root of scaffold project',
+        canSelectFiles: false,
+        canSelectFolders: true
+      };
+      const cwd = await vscode
+        .window
+        .showOpenDialog(options);
+      if (!cwd || !cwd[0]) {
+        return;
+      }
+
       if (item === 'init (default)') {
-        spawnChan(`anchor`, ['init', name], 'Scaffolding Anchor Project');
+        await spawnChan(`anchor`, ['init', name], 'Scaffolding Anchor Project', cwd[0].fsPath);
       } else if (item === 'Synthetify/solana-template') {
         try {
-          spawnChan(`npx`, ['degit', `https://github.com/${item}`, name], 'Scaffolding Anchor Project');
-          vscode.window.showInformationMessage(`Anchor ⚓: init Synthetify template completed!`);
+          await spawnChan(`npx`, ['degit', `https://github.com/${item}`, name], 'Scaffolding Anchor Project', cwd[0].fsPath);
         } catch (err) {
-          vscode.window.showErrorMessage(`Anchor ⚓: init Synthetify template faled!`);
+          vscode.window.showErrorMessage(`Anchor ⚓: init Synthetify template failed!`);
           if (err instanceof Error) {
             chan.append(err?.message);
             chan.show(true);
           }
         }
       }
+
+      const openInWorkspace = await vscode
+        .window
+        .showWarningMessage('Jump into the Anchor project?', 'yesss lfg', 'later maybe');
+
+      if (openInWorkspace === 'yesss lfg') {
+        const uri = vscode.Uri.file(path.join(`${cwd[0].fsPath}`,`${name}`));
+        await vscode.commands.executeCommand('vscode.openFolder', uri);
+        await vscode.window.showInformationMessage(`Anchor ⚓: Jumping to your ${name} anchor project!`);
+      }
+
+      return true;
+    } catch (err) {
+      if (err instanceof Error) {
+        chan.show(true);
+        appendChan('ERROR', err?.message);
+      };
+      return false;
     }
   }
 );
