79cea989c464d63a94cda2b03d44ee80ccced2f0
heyAyushh <ayush-15@live.com>
2022-02-27 23:50:42 +0530

    check Install Solana

--- src/helpers/install.ts
+++ src/helpers/install.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 import chan, { appendChan } from "./outputChannel";
 import { spawnChan } from "./spawnExec";
 import { checkAvm, installAnchorUsingAvm, installAvm } from "./avm";
+import pwshChan from "./pwshChan";
 
 export default async function checkInstallAnchor() {
   try {
@@ -40,3 +41,42 @@ export default async function checkInstallAnchor() {
     }
   }
 }
+
+export const checkInstallSolana = async () => {
+  try {
+    await spawnChan('solana', ['--version'], 'solana version', '', true);
+  } catch (err) {
+    const version = 'v1.9.9';
+    const isWin = process.platform === "win32";
+
+    if (!isWin) {
+      vscode.window.withProgress({
+        location: vscode.ProgressLocation.Notification,
+        title: `Installing Solana ...`,
+        cancellable: false
+      }, async (progress, token) => {
+        await spawnChan('sh', ['-c', `"$(curl -sSfL https://release.solana.com/v1.9.9/install)"`], 'solana install', '', true);
+      });
+    } else {
+      const exeFile = 'solana-install-init-x86_64-pc-windows-msvc.exe';
+      const SOLANA_RELEASE_URL = 'https://release.solana.com';
+      const tmpPath = 'C:\solana-install-tmp\solana-install-init.exe';
+
+      vscode.window.withProgress({
+        location: vscode.ProgressLocation.Notification,
+        title: `Downloading Solana Install toolkit ...`,
+        cancellable: false
+      }, async (progress, token) => {
+        await pwshChan('curl', [`${SOLANA_RELEASE_URL}/${version}/${exeFile}`, '--output', tmpPath, '--create-dirs'], 'solana install', '', true);
+      }).then(() =>
+        vscode.window.withProgress({
+          location: vscode.ProgressLocation.Notification,
+          title: `Installing Solana ${version} ...`,
+          cancellable: false
+        }, async (progress, token) => {
+          await pwshChan('curl', [`${SOLANA_RELEASE_URL}/${version}/${exeFile}`, '--output', tmpPath, '--create-dirs'], 'solana install', '', true);
+        })
+      );
+    }
+  }
+};
